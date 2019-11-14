3e0b9a9b3b5e8d198a12cfa5e0d633c90adb6a58
MapKo88 <mapko@email.ua>
2019-11-14 19:37:17 +0200

    add

---
+++ backend.php
@@ -0,0 +1,19 @@
+<?php 	 
+if($_GET['a'])
+{
+	echo "a = " . $_GET['a'] . "<br/>";
+}
+else
+{
+	echo "a = no parameter <br/>";
+}
+
+if($_GET['b'])
+{
+	echo "b = " . $_GET['b'];
+}
+else
+{
+	echo "b = no parameter";
+}
+?>
